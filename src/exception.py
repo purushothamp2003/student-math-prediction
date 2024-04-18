3d00529194b64306cf153c82aea42cc255cb6177
purushothamp2003 <2021cs_purushothamp_b@nie.ac.in>
2024-04-18 17:22:40 +0530

    EDA and Model training added

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+from src.logger import logging
 
 def error_message_details(error , error_details:sys):
     _ , _ , exc_tb = error_details.exc_info()
@@ -18,10 +18,9 @@ class CustomException(Exception):
     def __str__(self) -> str:
         return self.error_message
     
-# if __name__ == '__main__':
-
-#     try:
-#         a = 1 / 0
-#     except Exception as e:
-#         logging.info("Logging Has Started")
-#         raise CustomException(e , sys)
+if __name__ == '__main__':
+    try:
+        a = 1 / 0
+    except Exception as e:
+        logging.info("Logging Has Started")
+        raise CustomException(e , sys)
