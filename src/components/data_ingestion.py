0ee1fa1cc6167be7780b4623e8e70ec5b4f10253
purushothamp2003 <2021cs_purushothamp_b@nie.ac.in>
2024-04-26 13:14:55 +0530

    Model Training Completed

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -6,6 +6,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 from src.components.data_transformation import DataTransformation , DataTransformationConfig
+from src.components.model_trainer import ModelTrainer , ModelTrainerConfig
 
 
 @dataclass
@@ -53,6 +54,9 @@ if __name__ == "__main__":
     train_data , test_data=obj.initiate_data_ingestion()
 
     data_transformation = DataTransformation()
-    data_transformation.initiate_data_transformation(train_data ,  test_data)
+    train_array , test_array , _ = data_transformation.initiate_data_transformation(train_data ,  test_data)
 
+    modeltrainer = ModelTrainer()
+    r2_scoree = modeltrainer.initiate_model_training(train_array , test_array )
 
+    print(r2_scoree)
